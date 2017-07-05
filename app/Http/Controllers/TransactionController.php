a48a058a15693d494a331959495a0fda681769b6
Vytautas Jašinskas <j.vytautas09@gmail.com>
2017-07-05 20:15:34 +0300

    Legal users can cash out with taxes & currencies

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -15,38 +15,90 @@ class TransactionController extends Controller
 
     public function store(Request $request)
     {
-        // Getting money & adding taxes
-        $income = request('amount');
-        $tax = $income * 0.003;
-        if ($tax > 5){
-            $tax = 5;
+        /*
+         * Checking that was the operation
+         *
+         * Operation -> Cash In
+         */
+        if (request('operation') === 'cash_in'){
+
+            // Getting money & adding taxes
+            $income = request('amount');
+            $tax = $income * 0.003;
+            if ($tax > 5){
+                $tax = 5;
+            }
+            $amount = $income - $tax;
+
+            // Creating transaction
+            Transaction::create([
+                'amount' => $amount,
+                'currency' => request('currency'),
+                'user_id' => Auth()->user()->id,
+                'operation' => request('operation'),
+                'tax' => $tax,
+            ]);
+
+            // Checking the currency & converting to EUR
+            $currency = request('currency');
+            if ($currency === 'JPY'){
+                $amount = $amount / 129.53;
+            } elseif ($currency === 'USD'){
+                $amount = $amount / 1.1497;
+            }
+
+            // Add money to your current account
+            $balance = Auth()->user()->amount;
+            $totalAmount = $balance + $amount;
+
+            // Updating user account in DB
+            $id = Auth()->user()->id;
+            $user = User::find($id);
+            $user->amount = $totalAmount;
+            $user->save();
+
+        } else {
+            /*
+             * Operation -> Cash Out
+             */
+
+            // Amount of money willing to take
+            $cashout = request('amount');
+
+            // Adding taxes
+            $tax = $cashout * 0.03;
+            if ($tax < 0.5){
+                $tax = 0.5;
+            }
+            $amount = $cashout + $tax;
+
+            // Creating transaction
+            Transaction::create([
+                'amount' => $cashout,
+                'currency' => request('currency'),
+                'user_id' => Auth()->user()->id,
+                'operation' => request('operation'),
+                'tax' => $tax,
+            ]);
+
+            // Checking the currency & converting to EUR
+            $currency = request('currency');
+            if ($currency === 'JPY'){
+                $amount = $amount / 129.53;
+            } elseif ($currency === 'USD'){
+                $amount = $amount / 1.1497;
+            }
+
+            // Subtract money from your current account
+            $balance = Auth()->user()->amount;
+            $totalAmount = $balance - $amount;
+
+            // Updating user account in DB
+            $id = Auth()->user()->id;
+            $user = User::find($id);
+            $user->amount = $totalAmount;
+            $user->save();
         }
-        $amount = $income - $tax;
-
-        // Creating transaction
-        Transaction::create([
-            'amount' => $amount,
-            'currency' => request('currency'),
-            'user_id' => Auth()->user()->id,
-        ]);
-
-        // Checking the currency & converting to EUR
-        $currency = request('currency');
-        if ($currency === 'JPY'){
-            $amount = $amount / 129.53;
-        } elseif ($currency === 'USD'){
-            $amount = $amount / 1.1497;
-        }
-
-        // Add money to your current account
-        $balance = Auth()->user()->amount;
-        $totalAmount = $balance + $amount;
-
-        // Updating user account in DB
-        $id = Auth()->user()->id;
-        $user = User::find($id);
-        $user->amount = $totalAmount;
-        $user->save();
 
         // Redirecting to Home page
         return redirect('/home');
--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,29 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
+// Welcome page
 Route::get('/', function () {
     return view('welcome');
 });
-
+// Admin panel
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
-
+// Authentication
 Auth::routes();
-
 //Home page --- Cash In & Cash Out
 Route::get('/home', 'HomeController@index')->name('home');
 //Cash In
 Route::get('/in', function () {
     return view('in');
 });
-Route::post('/transaction', 'transactionController@store');
-Route::get('/in', 'HomeController@amount');
-Route::post('/in', 'HomeController@store');
 //Cash Out
 Route::get('/out', function () {
     return view('out');
 });
+// Transactions
+Route::post('/transaction', 'transactionController@store');
+
+
+
+///////// Neaišku ar reikalingi
+Route::get('/in', 'HomeController@amount');
+Route::post('/in', 'HomeController@store');
