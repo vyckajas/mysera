adb5b852d097984c4d23dac8fd07b891bea9ef8a
Vytautas Jašinskas <j.vytautas09@gmail.com>
2017-07-05 15:21:51 +0300

    cash saves to db with taxes & account with all currencies

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -15,29 +15,40 @@ class TransactionController extends Controller
 
     public function store(Request $request)
     {
-
+        // Getting money & adding taxes
         $income = request('amount');
-        $tax = $income * 0.0003;
+        $tax = $income * 0.003;
         if ($tax > 5){
             $tax = 5;
         }
         $amount = $income - $tax;
 
-        $balance = Auth()->user()->amount;
-        $totalAmount = $balance + $amount;
-
-        $id = Auth()->user()->id;
-
-        $user = User::find($id);
-        $user->amount = $totalAmount;
-        $user->save();
-
+        // Creating transaction
         Transaction::create([
             'amount' => $amount,
             'currency' => request('currency'),
             'user_id' => Auth()->user()->id,
         ]);
 
+        // Checking the currency & converting to EUR
+        $currency = request('currency');
+        if ($currency === 'JPY'){
+            $amount = $amount / 129.53;
+        } elseif ($currency === 'USD'){
+            $amount = $amount / 1.1497;
+        }
+
+        // Add money to your current account
+        $balance = Auth()->user()->amount;
+        $totalAmount = $balance + $amount;
+
+        // Updating user account in DB
+        $id = Auth()->user()->id;
+        $user = User::find($id);
+        $user->amount = $totalAmount;
+        $user->save();
+
+        // Redirecting to Home page
         return redirect('/home');
     }
 }
